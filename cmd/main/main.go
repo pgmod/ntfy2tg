19214f99af51f56e6248696cd808ab0f001962fe
guhbap <guhbap@gmail.com>
2025-04-27 23:43:42 +0300

    Update dependencies and refactor Telegram bot integration. Replace deprecated `go-telegram-bot-api` with `go-telegram/bot`, streamline message sending, and enhance message structure with additional fields.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -8,17 +8,20 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
 	"github.com/gorilla/websocket"
-	"github.com/nikoksr/notify"
-	"github.com/nikoksr/notify/service/telegram"
 	"github.com/pgmod/envconfig"
 )
+
+var tgbot *bot.Bot
+var TG_CHAT_ID int64
 
 func main() {
 	envconfig.Load()
 
 	TG_BOT_TOKEN := envconfig.Get("TG_BOT_TOKEN", "")
-	TG_CHAT_ID := envconfig.GetInt64("TG_CHAT_ID", 0)
+	TG_CHAT_ID = envconfig.GetInt64("TG_CHAT_ID", 0)
 	NTFY_ADDRS, err := envconfig.ToList(envconfig.Get("NTFY_ADDRS", ""), ",")
 	if err != nil {
 		log.Fatal(err)
@@ -30,15 +33,15 @@ func main() {
 	if TG_CHAT_ID == 0 {
 		log.Fatal("TG_CHAT_ID is not set")
 	}
-	telegramService, _ := telegram.New(TG_BOT_TOKEN)
-	telegramService.AddReceivers(TG_CHAT_ID)
+	tgbot, _ = bot.New(TG_BOT_TOKEN)
+	go tgbot.Start(context.Background())
 
-	notify.UseServices(telegramService)
-
-	_ = notify.Send(
+	tgbot.SendMessage(
 		context.Background(),
-		"Запуск бота",
-		"Бот запущен для тем: \n"+strings.Join(NTFY_ADDRS, "\n"),
+		&bot.SendMessageParams{
+			ChatID: TG_CHAT_ID,
+			Text:   "Бот запущен для тем: \n" + strings.Join(NTFY_ADDRS, "\n"),
+		},
 	)
 
 	wg := sync.WaitGroup{}
@@ -53,12 +56,16 @@ func main() {
 }
 
 type Message struct {
-	ID      string `json:"id"`
-	Time    int64  `json:"time"`
-	Expires int64  `json:"expires"`
-	Event   string `json:"event"`
-	Topic   string `json:"topic"`
-	Message string `json:"message"`
+	ID          string   `json:"id"`
+	Time        int64    `json:"time"`
+	Expires     int64    `json:"expires"`
+	Event       string   `json:"event"`
+	Topic       string   `json:"topic"`
+	Message     string   `json:"message"`
+	Priority    int      `json:"priority"`
+	Tags        []string `json:"tags"`
+	Title       string   `json:"title"`
+	ContentType string   `json:"content_type"`
 }
 
 func listen(addr string) {
@@ -71,16 +78,27 @@ func listen(addr string) {
 			log.Fatal(err)
 		}
 		var msg Message
+		fmt.Println(string(data))
 		err = json.Unmarshal(data, &msg)
 		if err != nil {
 			log.Fatal(err)
 		}
 		if messageType == websocket.TextMessage {
 			if msg.Event == "message" {
-				err = notify.Send(
+				var pm *models.ParseMode
+				if msg.ContentType == "text/markdown" {
+					p := models.ParseModeMarkdown
+					pm = &p
+				} else {
+					pm = nil
+				}
+				_, err = tgbot.SendMessage(
 					context.Background(),
-					msg.Topic,
-					msg.Message,
+					&bot.SendMessageParams{
+						ChatID:    TG_CHAT_ID,
+						Text:      tagsToEmoji(msg.Tags) + " " + msg.Title + "\n" + msg.Message,
+						ParseMode: *pm,
+					},
 				)
 				if err != nil {
 					log.Fatal(err)
@@ -91,4 +109,15 @@ func listen(addr string) {
 			ws, _, _ = websocket.DefaultDialer.Dial("wss://"+addr+"/ws", nil)
 		}
 	}
+}
+
+func tagsToEmoji(tags []string) string {
+	result := ""
+	for _, tag := range tags {
+		if emoji, ok := Emoji[tag]; ok {
+			result += emoji
+		}
+	}
+	fmt.Println("Emoji:", result)
+	return result
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,9 @@ module ntfy2tg
 go 1.23.6
 
 require (
+	github.com/go-telegram/bot v1.14.2
 	github.com/gorilla/websocket v1.5.3
 	github.com/pgmod/envconfig v0.1.0
 )
 
-require (
-	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/nikoksr/notify v1.3.0 // indirect
-	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	golang.org/x/sync v0.13.0 // indirect
-)
+require github.com/joho/godotenv v1.5.1 // indirect
