1d3d2f536730079048d375c048ec5aba540fd1a0
guhbap <guhbap@gmail.com>
2025-04-27 22:23:50 +0300

    init

---
+++ cmd/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"strings"
+	"sync"
+
+	"github.com/gorilla/websocket"
+	"github.com/nikoksr/notify"
+	"github.com/nikoksr/notify/service/telegram"
+	"github.com/pgmod/envconfig"
+)
+
+func main() {
+	envconfig.Load()
+
+	TG_BOT_TOKEN := envconfig.Get("TG_BOT_TOKEN", "")
+	TG_CHAT_ID := envconfig.GetInt64("TG_CHAT_ID", 0)
+	NTFY_ADDRS, err := envconfig.ToList(envconfig.Get("NTFY_ADDRS", ""), ",")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if TG_BOT_TOKEN == "" {
+		log.Fatal("TG_BOT_TOKEN is not set")
+	}
+	if TG_CHAT_ID == 0 {
+		log.Fatal("TG_CHAT_ID is not set")
+	}
+	telegramService, _ := telegram.New(TG_BOT_TOKEN)
+	telegramService.AddReceivers(TG_CHAT_ID)
+
+	notify.UseServices(telegramService)
+
+	_ = notify.Send(
+		context.Background(),
+		"Запуск бота",
+		"Бот запущен для тем: "+strings.Join(NTFY_ADDRS, ", "),
+	)
+
+	wg := sync.WaitGroup{}
+	for _, addr := range NTFY_ADDRS {
+		wg.Add(1)
+		go func(addr string) {
+			defer wg.Done()
+			listen(addr)
+		}(addr)
+	}
+	wg.Wait()
+}
+
+type Message struct {
+	ID      string `json:"id"`
+	Time    int64  `json:"time"`
+	Expires int64  `json:"expires"`
+	Event   string `json:"event"`
+	Topic   string `json:"topic"`
+	Message string `json:"message"`
+}
+
+func listen(addr string) {
+	ws, _, _ := websocket.DefaultDialer.Dial("wss://"+addr+"/ws", nil)
+	for {
+
+		messageType, data, err := ws.ReadMessage()
+		if err != nil {
+			log.Fatal(err)
+		}
+		var msg Message
+		err = json.Unmarshal(data, &msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if messageType == websocket.TextMessage {
+			if msg.Event == "message" {
+				err = notify.Send(
+					context.Background(),
+					msg.Topic,
+					msg.Message,
+				)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		} else if messageType == websocket.CloseMessage {
+			log.Println("Connection closed")
+			ws, _, _ = websocket.DefaultDialer.Dial("wss://"+addr+"/ws", nil)
+		}
+	}
+}
---
+++ dockerfile
@@ -0,0 +1,33 @@
+# Stage 1: Builder
+FROM golang:1.24.2-alpine AS builder
+
+# Аргумент для inline кэша (передаётся через --build-arg)
+ARG BUILDKIT_INLINE_CACHE=0
+
+# Устанавливаем рабочую директорию
+WORKDIR /app
+
+# Сначала копируем файлы зависимостей, чтобы не пересобирать их при изменении исходного кода
+COPY go.mod go.sum ./
+RUN go mod download
+
+
+# Копируем исходный код приложения и статические файлы
+COPY cmd cmd
+COPY pkg pkg
+COPY internal internal
+
+# Сборка приложения с использованием кэша Go (mount типа cache доступен при использовании BuildKit)
+RUN --mount=type=cache,target=/gocache \
+GOCACHE=/gocache \
+GOOS=linux GOARCH=amd64 \
+go build -ldflags="-w -s" -o app ./cmd/main/
+
+
+# Stage 2: Final image
+FROM bash:4.4.23
+
+# Копируем собранный бинарник и статические файлы из стадии builder
+COPY --from=builder /app/app /app/app
+
+CMD ["./app"]
